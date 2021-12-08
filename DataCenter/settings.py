eb3f5d1f5b1bbcfa67538cbbfbb2b0afbc969a78
saikumar110 <sai999694@gmail.com>
2021-12-08 22:04:30 +0530

    first commit

--- DataCenter/settings.py
+++ DataCenter/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_FINDERS = (
